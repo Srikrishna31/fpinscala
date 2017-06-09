d014751311d69beab5530af3fc7ce207e9c7d9e6
Srikrishna31 <coolkrishna31@gmail.com>
2017-06-09 14:36:28 +0530

    Implemented working on exercies for fpinscala book.

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,17 +1,58 @@
 package fpinscala.parsing
 
 import language.higherKinds
+import fpinscala.testing._
 
-trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+trait Parsers[ParseError, Parser[+_]] {
+  self =>
+  //THis introduces the name self to refer to this Parsers instance;
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
-  case class ParserOps[A](p: Parser[A]) {
+  def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
 
+  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
 
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+  def many[A](p: Parser[A]): Parser[List[A]]
+
+  def map[A,B](a: Parser[A])(f: A => B) : Parser[B]
+  //Why cant we add it directly to the trait?
+//  def |[B >:A](p2: Parser[B]) : Parser[B] = or(this, p2)
+
+  def slice[A](p: Parser[A]): Parser[String]
+
+  implicit def string(s: String): Parser[String]
+
+  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  case class ParserOps[A](p: Parser[A])
+  {
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2) //Use self to explicitly disambiguate reference to the or method on trait.
+    def or[B >: A](p2: Parser[B]) : Parser[B] = self.or(p, p2)
   }
 
-  object Laws {
+  object Laws
+  {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop = forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]) : Prop = equal(p, p.map(_))(in)
   }
 }
+//trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+//
+//  case class ParserOps[A](p: Parser[A]) {
+//
+//
+//  }
+//
+//  object Laws {
+//  }
+//}
 
 case class Location(input: String, offset: Int = 0) {
 
@@ -31,4 +72,16 @@ case class Location(input: String, offset: Int = 0) {
 
 case class ParseError(stack: List[(Location,String)] = List(),
                       otherFailures: List[ParseError] = List()) {
+}
+
+
+
+
+object Main extends App
+{
+  import fpinscala.parsing._
+  import fpinscala.parsing.Parsers
+
+
+  val numA: Parser[Int] = char('a').many.map(_.size)
 }
--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,35 +1,195 @@
 package fpinscala.testing
 
-import fpinscala.laziness.Stream
+//import fpinscala.laziness.Stream
+import fpinscala.parallelism.Par
 import fpinscala.state._
-import fpinscala.parallelism._
-import fpinscala.parallelism.Par.Par
+//import fpinscala.parallelism._
+//import fpinscala.parallelism.Par.Par
 import Gen._
 import Prop._
 import java.util.concurrent.{Executors,ExecutorService}
 
+package object StreamExtend
+{
+  implicit class StreamExtension[A](val s: Stream[A]) extends AnyVal
+  {
+    def unfold[A, S](z: S)(f: S => Option[(A, S)]) : Stream[A] = f(z) match
+    {
+      case None => Stream.Empty
+      case Some((a, s)) => Stream.cons(a, unfold(s)(f))
+    }
+  }
+}
+
+import StreamExtend._
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
 
-trait Prop {
+sealed trait Result
+{
+  def isFalsified : Boolean
+}
+
+case object Passed extends Result
+{
+  def isFalsified = false
+}
+
+case object Proved extends Result
+{
+  def isFalsified = false
+}
+
+case class Falsified(failure: FailedCase, successes : SuccessCount) extends Result
+{
+  def isFalsified = true
+}
+
+case class Prop(run: (MaxSize, TestCases, RNG) => Result)
+{
+  def forAll[A](as: Gen[A])(f: A => Boolean) : Prop = Prop {
+    (m, n, rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map{
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { case e : Exception => Falsified(buildMsg(a, e), i)}
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  def &&(p: Prop) : Prop = Prop {
+    (m, n, rng) => run(m, n, rng) match
+    {
+      case Passed => p.run(m, n, rng)
+      case x => x
+    }
+  }
+
+  def ||(p: Prop) : Prop = Prop{
+    (m, n, rng) => run(m, n, rng) match
+    {
+      case Falsified(failure, _) => p.run(m, n, rng)
+      case x => x
+    }
+  }
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean) : Prop = Prop
+  {
+    (max, n, rng) =>
+      val casesPerSize = (n + (max - 1)) / max
+      val props : Stream[Prop] = Stream.from(0).take((n min max) + 1).map(i => forAll(g(i))(f))
+      val prop: Prop = props.map(p => Prop { (max, _, rng) =>
+        p.run(max, casesPerSize, rng)}).toList.reduce(_ && _)
+      prop.run(max, n, rng)
+  }
+
+  /* Just a nuance of the pimp my library idiom to add an extra function to stream */
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] = Stream.Empty.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception) =
+    s"test case: $s\n" +
+    s"generated an exception: ${e.getMessage} \n" +
+    s"stack trace: \n ${e.getStackTrace.mkString("\n")}"
+
+  def check(p: => Boolean) : Prop =
+  {
+    lazy val result = p
+    forAll(unit(()))(_ => result)
+  }
+  //def &&(p: Prop) : Prop = new Prop { def check = Prop.this.check && p.check }
 }
 
 object Prop {
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+
+  type SuccessCount = Int
+  type TestCases = Int
+  type FailedCase = String
+  type MaxSize = Int
+  type Result = Option[(FailedCase, SuccessCount)]
+
+  def run(p: Prop,
+          maxSize: Int = 100,
+          testCases: Int = 100,
+          rng: RNG = RNG.Simple(System.currentTimeMillis)) : Unit =
+    p.run(maxSize, testCases, rng) match {
+      case Falsified(msg, n) =>
+        println(s"! Falsified after $n passed tests: \n$msg")
+      case Passed =>
+        println(s"+ OK, passed $testCases tests")
+      case Proved =>
+        println(s"+ OK, proved property")
+    }
+
+  def forAll[A](g: SGen[A])(f: A => Boolean) : Prop = forAll(g(_))(f)
+
+  val smallInt = Gen.choose(-10, 10)
+
+  val maxProp = forAll(SGen.listOf(smallInt, Gen(State(RNG.nonNegativeInt))) {
+      ns =>
+        val max  = ns.max
+        !ns.exists(_ > max)
+    }
+
+  val sortedProp = forAll(listOf(smallInt))
+
+  val ES: ExecutorService = Executors.newCachedThreadPool
+  val p1 = Prop.forAll(Gen.unit(Par.unit(1)))(i => Par.map(i)(_ + 1)(ES).get == Par.unit(2)(ES).get)
+
+}
+
+case class Gen[A](sample: State[RNG, A])
+{
+  def map[A,B](f: A => B) : Gen[B] = Gen(sample.map(f(_)))
+  def flatMap[A,B](f: A => Gen[B]) : Gen[B] = Gen(sample.flatMap((f(_).sample)))
+
+  def union[A](g1: Gen[A], g2: Gen[A]) : Gen[A] = boolean.flatMap((b:Boolean) => if (b) g1 else g2)
+
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)) : Gen[A] =
+  {
+    val g1Prob = g1._2.abs / (g1._2.abs + g2._2.abs)
+    Gen(State(RNG.double).flatMap(v => if (v < g1Prob) g1._1.sample else g2._1.sample))
+  }
+
+  def unsized : SGen[A] =  SGen(i => this)
 }
 
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] = Gen(State((a, _)))
+
+
+  def boolean: Gen[Boolean] = Gen(State(RNG.nonNegativeInt).map(a => if (a != 0) true else false))
+
+  def listOfN[A](n: Int, g: Gen[A]) : Gen[List[A]] = ??? //RNG.sequence(List.fill(n)(g.sample))
+
+  def choose(start: Int, stopExclusive: Int) : Gen[Int] = Gen(State((rng: RNG) =>
+  {
+    val next = rng.nextInt
+    ((next._1 - Int.MinValue) / (Int.MaxValue - Int.MinValue) * (stopExclusive + 1 - start), next._2)
+  }))
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+case class SGen[+A](forSize: Int => Gen[A])
+{
+  def apply(n: Int) : Gen[A] = forSize(n)
+
+  def map[A, B](f: A => B) : SGen[B] = SGen(forSize(_).map(f))
+
+  def flatMap[A, B](f: A => SGen[B]) : SGen[B] = SGen(n => forSize(n).flatMap((a: A) => f(a).forSize(n)))
+
+  //def **[B](s2: Gen[B]): SGen[(A,B)] = SGen(n => apply(n) ** s2(n))
+
+
+  def listOf1[A](g: Gen[A]) : SGen[List[A]] = SGen(n => Gen.listOfN(n max 1, g)) //define a list of 1
+
 }
 
-trait SGen[+A] {
+object SGen
+{
+  def listOf[A](g: Gen[A]): SGen[List[A]] = SGen(n => Gen.listOfN(n, g))
+}
+object Main extends App
+{
+
 
 }
 
